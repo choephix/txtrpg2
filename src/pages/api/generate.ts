60d5a258d5b93b76cd57a3f0fb4f06438db03a36
Stefan <choephix@gmail.com>
2024-07-27 20:23:49 +0000

    ++

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,15 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { generateObject } from 'ai';
+import { z } from 'zod';
+
 import { generateText } from 'ai';
 // import { google } from '@ai-sdk/google';
 import { createOpenAI as createGroq } from '@ai-sdk/openai';
 import { StringConstants } from '@/lib/defaultHistory';
-// import { createGoogleGenerativeAI as createGroq } from '@ai-sdk/google';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
 
 const groq = createGroq({
   baseURL: 'https://api.groq.com/openai/v1',
   apiKey: process.env.GROQ_API_KEY,
+});
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_AI_STUDIO_API_KEY,
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
@@ -18,13 +25,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const { prompt } = req.query || {};
   if (!prompt) return res.status(400).json({ error: 'No prompt provided' });
 
-  const { text } = await generateText({
-    // model: groq('llama-3.1-8b-instant'),
-    model: groq('gemma2-9b-it'),
-    // model: google('models/gemini-1.5-flash-latest'),
-    prompt: String(prompt),
-    system: StringConstants.systemMessage,
+  const model = groq('gemma2-9b-it');
+  // const model = google('models/gemini-1.5-flash-latest', {});
+  // const model = groq('llama-3.1-8b-instant');
+
+  const schema = z.object({
+    text: z.string(),
   });
 
-  res.status(200).json({ text });
+  const { object } = await generateObject({
+    model: model,
+    schema: schema,
+    prompt: String(prompt),
+    system: StringConstants.systemMessage,
+    temperature: 0.3,
+  });
+
+  res.status(200).json(object);
 }
